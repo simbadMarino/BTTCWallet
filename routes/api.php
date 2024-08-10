91fa165518425a885e0f7796bf43ae4769ce4dae
adeelch9 <adeel.chaudhary@tron.network>
2024-08-10 03:19:40 -0400

    added backend api files

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['namespace'=>'Api'], function () {
-    Route::post('register', 'AuthController@register');
-    Route::post('login', 'AuthController@login');
-    
-    Route::group(['middleware' => ['auth:api']], function () {
-        Route::post('/logout', 'AuthController@logout');
-        Route::get('/products/search/{title}', 'ProductController@search');
-        Route::apiResource('products', 'ProductController');
-    });
-});
+Route::get('/balance/{wallet}', [ API::class, 'Balance'      ]);  // complete - check format
+Route::get('/transaction/{id}', [ API::class, 'Transaction'  ]);  // complete
+Route::any('/transactions',     [ API::class, 'Transactions' ]); // complete
+Route::post('/transfer',        [ API::class, 'Transfer'     ]);  // complete
+Route::post('/verify',          [ API::class, 'Verify'       ]);  // complete
+Route::post('/generate',        [ API::class, 'Generate'     ]);  // complete
+Route::get('/{any}',            [ API::class, 'Invalid'      ])->where([ 'any' => '.*' ]); // complete
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::any('/{any}', function () {
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    return response()->json([ 
+        'status' => 'error',
+        'error' => 'not-allowed',
+        'info' => 'Method is not allowed'
+    ], 405);
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+})->where([ 'any' => '.*' ]);
