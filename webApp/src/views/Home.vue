ac2dcd6c816238741e5e3620e9bd6d5150fbaf8d
adeelch9 <adeel.chaudhary@tron.network>
2024-08-10 05:58:07 -0400

    testing tron web app

--- webApp/src/views/Home.vue
+++ webApp/src/views/Home.vue
@@ -27,14 +27,14 @@
 
     const balance  = ref(null);
     const rank     = ref(null);
-    const currency = ref('BTTC');
+    const currency = ref('trx');
     const router   = useRouter();
     const ranks    = useRankStore();
 
     const wallet = ref(Utils.GetWallet('wallet'));
 
     let currencies = {
-        BTTC: 'BTTC',
+        trx: 'TRX',
         usdt: 'USDT',
     };
 
@@ -111,7 +111,7 @@
                 <h2 class="w650-dots-1">{{ $t('home.balance') }}</h2>
 
                 <ul>
-                    <li :class="[ 'w500-dots-1', currency === 'BTTC' ? 'active' : '' ]" @click="setCurrency('BTTC')">BTTC</li>
+                    <li :class="[ 'w500-dots-1', currency === 'trx' ? 'active' : '' ]" @click="setCurrency('trx')">TRX</li>
                 </ul>
             </div>
 
